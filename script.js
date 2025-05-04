9ba867a3f7058d2e00d766a56a7a16f92337d6ec
Houn-Sopheak <sopheakhoun429@gmail.com>
2025-05-04 10:52:04 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+
+    function change(){
+        let pic = "https://i.pinimg.com/736x/f7/40/c4/f740c4bc8900714d7f725ed2e43d0f3f.jpg";
+        document.getElementById('img').setAttribute('src',pic);
+
+    }
+
+    function change_1(){
+        let pic_1 = "https://i.pinimg.com/736x/fe/8d/c7/fe8dc7ce63045c8f0398299253b6845b.jpg";
+        document.getElementById('img').setAttribute('src',pic_1);
+    }
+
+    function change_2(){
+        let pic_2 = "https://i.pinimg.com/736x/39/d6/ff/39d6ff68f7b616f1151324e13fa6e60c.jpg";
+        document.getElementById('img').setAttribute('src',pic_2);
+    }
+
+    function change_3(){
+        let pic_3 = "https://i.pinimg.com/736x/98/ed/0a/98ed0a9bbb53510ea84694ac27f5fbd4.jpg";
+        document.getElementById('img').setAttribute('src',pic_3);
+    }
+
+    // switch_color
+    
+    function swich(){
+        let swich_color= "https://i.pinimg.com/736x/98/ed/0a/98ed0a9bbb53510ea84694ac27f5fbd4.jpg";
+        document.getElementById('img').setAttribute('src',swich_color);
+    }
+
+    function swich_1(){
+        let swich_color_1=  "https://i.pinimg.com/736x/f7/40/c4/f740c4bc8900714d7f725ed2e43d0f3f.jpg";
+        document.getElementById('img').setAttribute('src',swich_color_1);
+    }
+
+    
+    function swich_2(){
+        let swich_color_2=  "https://i.pinimg.com/736x/39/d6/ff/39d6ff68f7b616f1151324e13fa6e60c.jpg";
+        document.getElementById('img').setAttribute('src',swich_color_2);
+    }
+
+    function swich_3(){
+        let swich_color_3= "https://i.pinimg.com/736x/fe/8d/c7/fe8dc7ce63045c8f0398299253b6845b.jpg";
+        document.getElementById('img').setAttribute('src',swich_color_3);
+    }
+
